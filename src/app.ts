e0db14ed4eca4dc1981c7c598ee2ee8203206eff
prostack2003 <cool.deds@mail.ru>
2026-08-26 18:00:17 +0300

    add ts for app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import config from './config/index';
 import mealsRouter from './routes/meals.router';
 import errorHandler from './middleware/errorHandler';
@@ -6,11 +7,11 @@ import errorHandler from './middleware/errorHandler';
 
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 app.use('/api/meals', mealsRouter);
-app.use((req, res) => {
+app.use((req: Request, res: Response) => {
     res.status(404).json({ error: 'Not Found' });
 });
 app.use(errorHandler)
